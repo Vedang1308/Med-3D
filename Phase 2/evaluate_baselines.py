6377090ae2ac537f4f5e6f76782ac5ec237d3560
Vedang <vedangavaghade01@gmail.com>
2026-06-25 19:54:55 -0700

    Configure HuggingFace cache to use scratch directory

--- Phase 2/evaluate_baselines.py
+++ Phase 2/evaluate_baselines.py
@@ -5,6 +5,14 @@ import torch
 import random
 import traceback
 import numpy as np
+
+# Set HuggingFace cache to a scratch directory to avoid filling up the home directory
+scratch_dir = os.environ.get('SCRATCH', os.path.expanduser('~/scratch'))
+hf_cache = os.path.join(scratch_dir, 'huggingface_cache')
+os.makedirs(hf_cache, exist_ok=True)
+os.environ['HF_HOME'] = hf_cache
+os.environ['HF_DATASETS_CACHE'] = hf_cache
+os.environ['TRANSFORMERS_CACHE'] = hf_cache
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_dataset
 from huggingface_hub import hf_hub_download
